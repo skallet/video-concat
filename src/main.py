43e8638db984156f567743229a7e8a3140cf120e
skall <blazekm1lan@seznam.cz>
2018-11-04 12:50:12 +0100

    Added take limit argument

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ def print_help():
    print('\t-c <codec> # select output codec (libx264, mpeg4, rawvideo, png, libvorbis, libvpx)')
    print('\t-f <folder> # folder with video files to be edited')
    print('\t-o <output-file> # output file to store results')
+   print('\t-t <take-first> # if set, take only first N videos from random list')
    print('## word specified arguments:')
    print('\t--help # show this help')
    print('\t--folder=<folder> # folder with video files to be edited')
@@ -56,13 +57,14 @@ def main(argv):
    inputFolder = None
    outputFile = None
    codec = None
+   take = None
 
    print_short_licence()
 
    try:
       opts, args = getopt.getopt(
          argv,
-         "hf:o:c:",
+         "hf:o:c:t:",
          ["help", "folder=", "output="]
       )
    except getopt.GetoptError:
@@ -83,6 +85,8 @@ def main(argv):
          outputFile = arg
       elif opt in ("-c"):
          codec = arg
+      elif opt in ("-t"):
+         take = max(1, int(arg))
 
    if codec is not None and not codecs.has_key(codec):
       print('Unknown codec, use -h or --help to show help message.')
@@ -109,6 +113,10 @@ def main(argv):
       sys.exit(2)
 
    shuffle(videos)
+
+   if (take is not None):
+      videos = videos[:take]
+
    result = concatenate_videoclips(videos)
    result.write_videofile(outputFile, codec=codec)
 
